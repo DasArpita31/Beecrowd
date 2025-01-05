06a25b31f622adc33cbcc4e9c56fa81c7aa04298
Arpita Das <arpita1477@gmail.com>
2025-01-05 22:13:00 +0600

    1006

---
+++ Average 2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+     float A,B,C,Avg;
+     scanf("%f%f%f",&A,&B,&C);
+     Avg=(A*2+B*3+C*5)/(2+3+5);
+     printf("MEDIA = %.1lf\n",Avg);
+ 
+    return 0;
+}
