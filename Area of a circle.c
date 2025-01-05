37a83a607a228f05d43270a2740954066a195b80
Arpita Das <arpita1477@gmail.com>
2025-01-05 17:39:08 +0600

    1002

---
+++ Area of a circle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+     double R;
+     scanf("%lf",&R);
+    printf("A=%.4lf\n",(3.14159*(R*R)));
+    return 0;
+}
