da78a50f64f8fdd3305bae1e45e08893e0fa9d11
Arpita Das <arpita1477@gmail.com>
2025-01-05 17:19:04 +0600

    second problem

---
+++ Extremely Basic.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+     int A,B;
+     scanf("%d\n%d",&A,&B);
+     printf("X = %d\n",A+B);
+    return 0;
+}
