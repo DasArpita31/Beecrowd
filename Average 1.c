bf08b4c36e0633f8df558d458a7db538fe41310a
Arpita Das <arpita1477@gmail.com>
2025-01-05 22:00:58 +0600

    1005

---
+++ Average 1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+     double A,B,Avg;
+     scanf("%lf%lf",&A,&B);
+     Avg=(A*3.5+B*7.5)/(3.5+7.5);
+     printf("MEDIA = %.5lf\n",Avg);
+    return 0;
+}
