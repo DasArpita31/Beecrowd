98e81f80f2d8dbce9ce93fafbd22b33e00307eb7
Arpita Das <arpita1477@gmail.com>
2025-01-05 17:47:13 +0600

    1003

---
+++ simple sum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+     int A,B,sum;
+     scanf("%d\n%d",&A,&B);
+     sum=A+B;
+    printf("SOMA = %d\n",sum);
+    return 0;
+}
